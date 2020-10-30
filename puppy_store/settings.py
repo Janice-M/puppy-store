d730fbd6ca7c4c338f9e8d590403cca08599b371
Janice-M <janiceink001@gmail.com>
2020-10-30 18:25:46 +0300

    postgreSQL setup

--- puppy_store/settings.py
+++ puppy_store/settings.py
@@ -82,10 +82,13 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'frozenintime',
+    'USER': 'moringa',
+    'PASSWORD': '**kwasync',
+    
     }
-}
+    }
 
 
 # Password validation
