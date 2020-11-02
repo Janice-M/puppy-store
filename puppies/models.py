c64c115dcf06feb70cb852003649975fbd0a1209
Janice-M <janiceink001@gmail.com>
2020-11-02 22:33:16 +0300

    pupper

--- puppies/models.py
+++ puppies/models.py
@@ -10,3 +10,6 @@ class Puppy(models.model):
     color= models.CharField(max_length =255)
     created_at =models.DateTimeField(auto_now_add = True)
     updated_at = modesls.DateTimeField(auto_now = True)
+
+    def get_breed(self):
+        return self.name + 'belongs to' + self.breed + 'breed'
