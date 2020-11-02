1468e1321389020baf1a48181fc2080c43549a9b
Janice-M <janiceink001@gmail.com>
2020-11-02 23:17:24 +0300

    pupper test

--- puppies/tests.py
+++ puppies/tests.py
@@ -7,7 +7,7 @@ class PuppyTest(TestCase):
     
     def setUp(self):
         Puppy.objects.create(
-            name = 'Frigg' , age = 4 , breed = 'Bull Dog' ,color = 'Black'
+            name = 'Frigg' , age = 4 , breed = 'Bull ' ,color = 'Black'
         )
         Puppy.objects.create (
             name = 'Thor' , age = 2 , breed = 'Chihuahua', color = 'brown'
@@ -18,7 +18,7 @@ class PuppyTest(TestCase):
         puppy_thor = Puppy.objects.get(name = 'Thor')
         
         self.assertEqual (
-            puppy_frigg.get_breed(), "Frigg belongs to Bull Dog breed ."
+            puppy_frigg.get_breed(), "Frigg belongs to Bull breed ."
         )
         self.assertEqual(
             puppy_thor.get_breed(),  "Thor belongs to Chihuahua breed"
