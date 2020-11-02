8cae03b63b79aba63ba2f2dcb8a7e11d6d31d823
Janice-M <janiceink001@gmail.com>
2020-11-02 22:30:40 +0300

    pupper

--- puppies/models.py
+++ puppies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Puppy(models.model):
+    '''
+    THIS CLASS HOLDA OUR PUP ATTRTIBUTES
+    '''
+    name = models.CharField(max_length = 255)
+    age = models.IntegerField()
+    breed = models.CharField(max_length = 255)
+    color= models.CharField(max_length =255)
+    created_at =
