8cb2fdff1dddd594257035b515d639f0ab352af5
Janice-M <janiceink001@gmail.com>
2020-11-02 23:05:35 +0300

    pupper test

--- puppies/tests.py
+++ puppies/tests.py
@@ -2,3 +2,11 @@ from django.test import TestCase
 from . models import Puppy
 
 ''' test for puppy model'''
+
+class PuppyTest(TestCase):
+    
+    def setUp(self):
+        Puppy.Objects.create(
+            name = 'Frigg' , age = 4 , breed = 'Bull Dog' ,color = 'Black'
+        )
+        Pu
