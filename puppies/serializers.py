069988cc62a53ace40add7749d232565cfdac84a
Janice-M <janiceink001@gmail.com>
2020-11-04 21:15:24 +0300

    serializers

--- puppies/serializers.py
+++ puppies/serializers.py
@@ -4,4 +4,4 @@ from .models import Puppy
 class PuppySerializer (serializers.ModelSerializer):
     class Meta:
         model = Puppy
-        fields = ('name', 'ag', 'breed', 'color', 'created_at', 'updated_at')
+        fields = ('name', '', 'breed', 'color', 'created_at', 'updated_at')
