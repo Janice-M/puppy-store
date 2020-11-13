ec35e52009b54189c898d3fcec6a76dd3aee8224
Janice-M <janiceink001@gmail.com>
2020-11-13 23:41:07 +0300

    views

--- puppies/views.py
+++ puppies/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Puppy
+from .serializers import PuppySerializer
 
 # Create your views here.
