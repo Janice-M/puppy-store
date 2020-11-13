2de732a3727ed63dec6f4fa65936de4eaf872c62
Janice-M <janiceink001@gmail.com>
2020-11-13 23:47:07 +0300

    url in app

--- puppies/urls.py
+++ puppies/urls.py
@@ -1,2 +1,12 @@
 from django.conf.urls import url
 from . import views
+
+
+urlpatterns = [
+    url(
+        r'^api/v1/puppies/(?P<pk>[0-9]+)$',
+        views.get_delete_update_puppy,
+        name='get_delete_update_puppy'
+    ),
+    
+    
