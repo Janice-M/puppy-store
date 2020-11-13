1eb2d7f662fcd6d9ca7080bb7ae5f8db2352f8c0
Janice-M <janiceink001@gmail.com>
2020-11-13 23:47:26 +0300

    url in app

--- puppies/urls.py
+++ puppies/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
         views.get_delete_update_puppy,
         name='get_delete_update_puppy'
     ),
-    
+      url(
+        r'^api/v1/puppies/$',
+        views.get_post_puppies,
+        name='get_post_puppies'
+    )
+]
     
