7084a0f8950335bffe6e38631c3c1f4d33471fb2
Janice-M <janiceink001@gmail.com>
2020-11-25 23:46:19 +0300

    tests

--- puppies/tests/tests_views.py
+++ puppies/tests/tests_views.py
@@ -11,12 +11,4 @@ client = Client()
 
 class GetAllPuppiesTest(TestCase):
     """ Test module for GET all puppies API """
-    def setUp(self):
-        Puppy.objects.create(
-            name='Casper', age=3, breed='Bull Dog', color='Black')
-        Puppy.objects.create(
-            name='Muffin', age=1, breed='Gradane', color='Brown')
-        Puppy.objects.create(
-            name='Rambo', age=2, breed='Labrador', color='Black')
-        Puppy.objects.create(
-            name='Ricky', age=6, breed='Labrador', color='Brown')
+    
