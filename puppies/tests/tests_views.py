441b62856ab33d0b5b41e4ead63f2a6b0595badd
Janice-M <janiceink001@gmail.com>
2020-11-25 23:51:22 +0300

    test

--- puppies/tests/tests_views.py
+++ puppies/tests/tests_views.py
@@ -41,7 +41,7 @@ class GetSinglePuppyTest(TestCase):
         self.ricky = Puppy.objects.create(
             name='Ricky', age=6, breed='Labrador', color='Brown')
         
-     def test_get_valid_single_puppy(self):
+    def test_get_valid_single_puppy(self):
             response = client.get(
             reverse('get_delete_update_puppy', kwargs={'pk': self.rambo.pk}))
         puppy = Puppy.objects.get(pk=sel
