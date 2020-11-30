0fe4751021ff584546ab5b4e6f892479df9b46be
Janice-M <janiceink001@gmail.com>
2020-11-30 23:47:44 +0300

    invalid payload

--- puppies/tests/tests_views.py
+++ puppies/tests/tests_views.py
@@ -80,4 +80,10 @@ class CreateNewPuppyTest(TestCase):
             data=json.dumps(self.valid_payload),
             content_type='application/json'
         )
-         self.assertEqual(response.status_code, status.HTTP_201_CREATED
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+       def test_create_invalid_puppy(self):
+            response = client.post(
+            reverse('get_post_puppies'),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )  
